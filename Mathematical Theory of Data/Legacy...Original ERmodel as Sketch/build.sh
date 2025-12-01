cbb294c678c866f1c5e836e501ff0f58be3db88a
JohnWCartmell <john.w.cartmell@gmail.com>
2025-12-01 12:01:32 +0000

    reorg of the source for the original ERmodels as sketches paper

---
+++ Mathematical Theory of Data/Legacy...Original ERmodel as Sketch/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Ensure build and output folders exist
+mkdir -p build
+mkdir -p output
+
+# Sync source files into build folder
+rsync -a ./*.tex build/
+rsync -a ./latexmkrc build
+
+# Enter build folder quietly
+pushd build > /dev/null
+
+if [ -n "$1" ]; then
+    # User supplied a .tex filename
+    texfile="$1"
+
+    # Run latexmk on the given texfile
+    latexmk -pdfps "$texfile"
+
+    # Strip .tex to obtain output pdf name
+    pdfname="${texfile%.tex}.pdf"
+
+    # Copy PDF into output folder
+    cp "$pdfname" ../output/
+
+else
+    # No argument: build everything
+    latexmk -pdfps MToD.original.ERmodel.formalisedAsSketch
+
+    # Copy ALL generated PDFs into output/
+    cp *.pdf ../output/
+fi
+
+# Return to original directory quietly
+popd > /dev/null
+
